12dc5d2d8a9374e78602aa3bc54517c9e3042a01
tejas01g <gargtejasvi076@gmail.com>
2026-05-15 09:24:01 +0530

    n

---
+++ Practise/RemoveDuplicate.js
@@ -0,0 +1,16 @@
+function RemoveDuplicate(nums){
+    if (nums.length === 0) return 0;
+
+    let i = 0;
+
+    for (let j= 1; j < nums.length; j++){
+        if(nums[i]!= nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i+1;
+}
+let nums = [1,1,2,2,3,3,3,4,4,5,5];
+let k = RemoveDuplicate(nums);
+console.log(nums.slice(0,k))
