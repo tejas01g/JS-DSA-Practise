097969020e4e0befa7f8e392ff2b1182d538b058
tejas01g <gargtejasvi076@gmail.com>
2026-05-17 10:20:53 +0530

    n

--- threesum.js
+++ threesum.js
@@ -1,20 +1,45 @@
-function threesum(arr, target)
-{
-    for(let i = 0; i < arr.length; i++){
-        for(let j= i+1; j < arr.length; j++)
-            {
-            for(let k = j+1; k < arr.length; k++)
-                {
-                let sum = arr[i] + arr[j] + arr[k];
-                if (sum === target){
-                    return [arr[i] , arr[j] , arr[k]];
-                }
+// function threesum(arr, target)
+// {
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j= i+1; j < arr.length; j++)
+//             {
+//             for(let k = j+1; k < arr.length; k++)
+//                 {
+//                 let sum = arr[i] + arr[j] + arr[k];
+//                 if (sum === target){                                             //brute force 
+//                     return [arr[i] , arr[j] , arr[k]];
+//                 }
+//             }
+//         }
+//     }
+//     return "not found"
+// }
+
+// let arr = [2,4,5,6,7,8];
+// let target = 11;
+// console.log(threesum(arr,target))
+
+
+
+
+
+
+function threesum(arr, target){
+    for (let i = 0; i < arr.length; i++){
+        let map = new Map();
+
+        for (let j = i+1; j < arr.length; j++){
+            let compliment = target - (arr[i] + arr[j]);
+
+            if (map.has(compliment)){
+                return [arr[i] , compliment , arr[j]];
             }
+            map.set(arr[j], j)
         }
     }
     return "not found"
 }
 
-let arr = [2,4,5,6,7,8];
-let target = 11;
-console.log(threesum(arr,target))
+let arr = [1,2,0,5,8,9];
+let target = 12;
+console.log(threesum(arr, target))
