e9c9c70bc76e21742d2f6f5a55dec248e57b9434
tejas01g <gargtejasvi076@gmail.com>
2026-05-19 09:45:09 +0530

    n

--- threesum.js
+++ threesum.js
@@ -24,22 +24,70 @@
 
 
 
-function threesum(arr, target){
-    for (let i = 0; i < arr.length; i++){
-        let map = new Map();
+// function threesum(arr, target){
+//     for (let i = 0; i < arr.length; i++){
+//         let map = new Map();
 
-        for (let j = i+1; j < arr.length; j++){
-            let compliment = target - (arr[i] + arr[j]);
+//         for (let j = i+1; j < arr.length; j++){
+//             let compliment = target - (arr[i] + arr[j]);
 
-            if (map.has(compliment)){
-                return [i, map.get(compliment) , j];
+//             if (map.has(compliment)){
+//                 return [i, map.get(compliment) , j];
+//             }
+//             map.set(arr[j], j)
+//         }
+//     }
+//     return "not found"
+// }
+
+// let arr = [1,2,0,5,8,9];
+// let target = 12;
+// console.log(threesum(arr, target))
+
+
+
+
+
+//two pointer approach
+
+function threesum(nums){
+    nums.sort((a,b) => a-b);
+    let result = [];
+
+    for(let i = 0; i < nums.length-2; i++){
+        let left = i + 1;
+        let right = nums.length-1;
+
+        while(left < right){
+            let sum = nums[i] + nums[left] + nums[right];
+            
+            if(sum === 0){
+                result.push([nums[i], nums[left], nums[right]]);
+            
+            while(left < right && nums[left] === nums[left + 1]){
+                left++;
             }
-            map.set(arr[j], j)
+            while(left < right && nums[right] === nums[right - 1]){
+                right--;
+            } 
+        
+            left++;
+            right--;           
         }
+        else if( sum < 0){
+            left++;
+            }
+            else{
+                right--;
+            }
+        } 
+      
     }
-    return "not found"
-}
+    return result
+} 
 
-let arr = [1,2,0,5,8,9];
-let target = 12;
-console.log(threesum(arr, target))
+
+let nums = [-1, 0, 1, 2, -1, -4];
+
+console.log(threesum(nums));
+
