775b7bb78aedceea950fbbd5efc27fca028d909e
tejas01g <gargtejasvi076@gmail.com>
2026-05-28 09:44:10 +0530

    n

--- twosum.js
+++ twosum.js
@@ -1,33 +1,21 @@
-function twosum(nums,target){
-    let map = new Map();
+// function twosum(nums,target){
+//     let map = new Map();
 
-    for (let i = 0; i<nums.length; i++){
-        let compliment = target - nums[i];
-        if (map.has(compliment)){
-            return [map.get(compliment), i];
-        }
-        map.set(nums[i], i);
-    }
-}
+//     for (let i = 0; i<nums.length; i++){
+//         let compliment = target - nums[i];
+//         if (map.has(compliment)){
+//             return [map.get(compliment), i];
+//         }
+//         map.set(nums[i], i);
+//     }
+// }
 
-let nums = [2,4,3,9,7];
-let target = 11;
-let result = twosum(nums,target);
+// let nums = [2,4,3,9,7];
+// let target = 11;
+// let result = twosum(nums,target);
 
-console.log("Indices", result);
-console.log("values", nums[result[0]], nums[result[1]]);
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log("Indices", result);
+// console.log("values", nums[result[0]], nums[result[1]]);
 
 
 
@@ -98,3 +86,30 @@ console.log("values", nums[result[0]], nums[result[1]]);
 // let nums = [1,3,5,4,8];
 // let target = 9;
 // console.log ( twosum(nums,target))
+
+
+
+
+
+
+
+
+
+
+
+
+function twosum(nums, target){
+    let map = new Map();
+
+    for(let i = 0; i< nums.length; i++){
+        let compliment = target - nums[i];
+        if(map.has(compliment)){
+            return [map.get(compliment) , i];
+        }
+        map.set(nums[i], i)
+    }
+}
+
+let nums = [1,3,5,6,3,2];
+let target = 9;
+console.log(twosum(nums, target));
