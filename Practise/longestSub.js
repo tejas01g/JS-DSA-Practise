033f6539cb50fe708f0894edc1fa786c3e2c5825
tejas01g <gargtejasvi076@gmail.com>
2026-06-12 20:48:53 +0530

    practise

--- Practise/longestSub.js
+++ Practise/longestSub.js
@@ -1,22 +1,64 @@
+// function longestSub(s,k){
+//     let low = 0;
+//     let high = 0;
+//     let res = 0;
+//     let freq = new Map();
+
+//     for(high = 0; high < s.length; high++) {
+//         (freq.set[high] , (freq.get(s[high]) || 0)+1);
+//     }
+
+//     while(freq.size > k){
+//         (freq.set(s[low]) , freq.get(s[low])-1);
+//     }
+//     if(freq.get(s[low]) === 0){
+//         freq.delete(s[low]);
+//     }
+//     low++;
+// }
+// if(freq.size === k){
+//     let len = high - low + 1;
+//     let res = (res, len);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function longestSub(s,k){
-    let low = 0;
     let high = 0;
+    let low = 0;
     let res = 0;
     let freq = new Map();
 
-    for(high = 0; high < s.length; high++) {
-        (freq.set[high] , (freq.get(s[high]) || 0)+1);
+    for (high = 0; high < s.length; high++) {
+        freq.set(s[high] , (freq.get(s[high]) || 0) +1);
+        while(freq.get(s[high] ) > 1) {
+            freq.set(s[low], freq.get(s[low]) -1);
+            if(freq.get(s[low]) === 0) {
+                freq.delete(s[low]);
+            }
+            low++;
+        }
+         let len = high - low + 1;
+        res = Math.max(res,len)
     }
+    return res
+}
 
-    while(freq.size > k){
-        (freq.set(s[low]) , freq.get(s[low])-1);
-    }
-    if(freq.get(s[low]) === 0){
-        freq.delete(s[low]);
-    }
-    low++;
-}
-if(freq.size === k){
-    let len = high - low + 1;
-    let res = (res, len);
-}
+let s = "aaaabbbccccdddeeeeeeff";
+let k = 2
+
+console.log(longestSub(s,k))
