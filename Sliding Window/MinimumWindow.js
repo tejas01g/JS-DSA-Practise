cee39c91c34d3ba620ff8e8a56f6e17b0222f803
tejas01g <gargtejasvi076@gmail.com>
2026-06-25 10:11:49 +0530

    n

--- Sliding Window/MinimumWindow.js
+++ Sliding Window/MinimumWindow.js
@@ -1,36 +1,78 @@
-//leetcode 76 - hard
+// //leetcode 76 - hard
 
-function MinimumWindow(s, t) {
-  let have = new Array(256).fill(0);
+// function MinimumWindow(s, t) {
+//   let have = new Array(256).fill(0);
+//   let need = new Array(256).fill(0);
+
+//   for (let ch of t) {
+//     need[ch.charCodeAt(0)]++;
+//   }
+
+//   let low = 0;
+//   let high = 0;
+//   let res = Infinity;
+//   let start = 0;
+
+//   for (high = 0; high < s.length; high++) {
+//     have[s[high].charCodeAt(0)]++;
+//     while (isValid(need, have)) {
+//       let len = high - low + 1;
+
+//       if (res > len) {
+//         res = len;
+//         start = low;
+//       }
+//       have[s[low].charCodeAt(0)]--;
+//       low++;
+//     }
+//   }
+//   return res === Infinity ? "" : s.substring(start, start + res);
+
+//   function isValid(need, have) {
+//     for (let i = 0; i < 256; i++) {
+//       if (have[i] < need[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+// }
+
+// let s = "aabaaabbbcbddd";
+// let t = "abc";
+// console.log(MinimumWindow(s, t));
+
+
+
+
+
+function MinimumWindow(s,t) {
   let need = new Array(256).fill(0);
+  let have = new Array(256).fill(0);
 
-  for (let ch of t) {
+  for(let ch of t) {
     need[ch.charCodeAt(0)]++;
   }
-
   let low = 0;
   let high = 0;
   let res = Infinity;
   let start = 0;
 
-  for (high = 0; high < s.length; high++) {
+  for(let high = 0; high < s.length; high++) {
     have[s[high].charCodeAt(0)]++;
-    while (isValid(need, have)) {
+    while(isValid(have,need)) {
       let len = high - low + 1;
 
-      if (res > len) {
+      if(res > len) {
         res = len;
         start = low;
       }
-      have[s[low].charCodeAt(0)]--;
-      low++;
+      return res === Infinity ? "" : s.substring(start , start + res);
     }
   }
-  return res === Infinity ? "" : s.substring(start, start + res);
-
-  function isValid(need, have) {
-    for (let i = 0; i < 256; i++) {
-      if (have[i] < need[i]) {
+  function isValid(have,need) {
+    for(let i = 0; i < 256; i++) {
+      if(have[i] < need[i]) {
         return false;
       }
     }
@@ -38,6 +80,7 @@ function MinimumWindow(s, t) {
   }
 }
 
-let s = "aabaaabbbcbddd";
-let t = "abc";
-console.log(MinimumWindow(s, t));
+let s = "aaaggbbhbccpcuiesbimnopqqquu"
+let t = "abc"
+
+console.log(MinimumWindow(s,t))
